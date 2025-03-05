4e62f835e95caa4427533c1852819eda890bbf4a
MR-NOKIB <nozomuddinnokib03@gmail.com>
2025-03-05 20:52:14 +0000

    cteated navber and banner

--- src/pages/shared/NavBar/NavBar.jsx
+++ src/pages/shared/NavBar/NavBar.jsx
@@ -4,17 +4,19 @@ const NavBar = () => {
     const navOptions = <>
         <li><a>Item 1</a></li>
         <li>
-            <a>Parent</a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
+            <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                </ul>
+            </details>
         </li>
         <li><a>Item 3</a></li>
     </>
     return (
         <>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar fixed z-10 bg-black/30 text-white max-w-screen-xl">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,7 +25,7 @@ const NavBar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                                {navOptions}
+                            {navOptions}
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">Bistro Boss</a>
